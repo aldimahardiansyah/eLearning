7d7a27eb2bdea5c5729f2c102579a0130f59a64a
aldimahardiansyah <aldimdiansyah@gmail.com>
2024-05-29 09:15:30 +0700

    praktik 10

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/**
+ * Method HTTP:
+ * 1. Get = menampilkan halaman
+ * 2. Post = mengirim data
+ * 3. Put = meng-update data
+ * 4. Delete = menghapus data
+ */
+
+// route untuk menampilkan teks
+Route::get('/salam/{nama}', function ($nama) {
+    return "Assalamualaikum $nama";
+});
+
+Route::get('admin/dashboard', [DashboardController::class, 'index']);
+
+// Route untuk menampilkan student
+Route::get('admin/student', [StudentController::class, 'index']);
